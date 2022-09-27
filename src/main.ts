5290f36a006567063ca1732f61aa596563974d9e
tempura327 <cks6658324@yahoo.com.tw>
2022-09-27 21:01:09 +0800

    chore: impored font-awesome-icon packages and index.css to main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,19 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import '@/style/index.css'
+
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
+
+library.add(fas);
+library.add(far);
+
+Vue.component('font-awesome-icon', FontAwesomeIcon);
+
 Vue.config.productionTip = false
 
 new Vue({
