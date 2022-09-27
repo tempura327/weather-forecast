4e0d68471681fefb4b376d0bcd7880c252d66eb3
tempura327 <cks6658324@yahoo.com.tw>
2022-09-27 20:59:09 +0800

    feat(component): added InputGroup and SearchBar

---
+++ src/components/InputGroup.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="group" :class="className">
+    <slot name="input-group-prepend" class="group_prepend"></slot>
+
+    <slot></slot>
+
+    <slot name="input-group-append" class="group_append"></slot>
+  </div>
+</template>
+
+<script lang="ts">
+  import { Component, Vue, Prop } from 'vue-property-decorator';
+
+  @Component
+  export default class InputGroup extends Vue {
+    // props
+    @Prop({ default: '' }) className?: string;
+    // data
+    // hook
+    // methods
+  }
+</script>
---
+++ src/components/SearchBar.vue
@@ -0,0 +1,38 @@
+<template>
+  <InputGroup :className="className">
+    <input v-model="keyword" class="group_prepend group_input" :class="{ 'group_input-block': isBlock }" type="text" />
+
+    <template #input-group-append>
+      <button class="btn group_append" @click="searchClick">
+        <font-awesome-icon class="text-white" icon="magnifying-glass" />
+      </button>
+    </template>
+  </InputGroup>
+</template>
+
+<script lang="ts">
+  import { Component, Vue, Prop } from 'vue-property-decorator';
+
+  import InputGroup from '@/components/InputGroup.vue';
+
+  @Component({
+    components: {
+      InputGroup,
+    },
+  })
+  export default class SearchBar extends Vue {
+    // props
+    @Prop({ default: '' }) className?: string;
+    @Prop({ default: true }) isBlock!: boolean;
+
+    // data
+    keyword = '';
+
+    // hook
+
+    // methods
+    searchClick(): void {
+      this.$emit('searchClick', this.keyword);
+    }
+  }
+</script>
