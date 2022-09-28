e2765423b6b7ca452f651b79cbb887347acfed6b
tempura327 <cks6658324@yahoo.com.tw>
2022-09-28 22:13:03 +0800

    feat(componet): added Spinner.

---
+++ src/components/Spinner.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="spinner" :class="`spinner-${size} text-${color} ${className}`">
+    <span>Loading...</span>
+  </div>
+</template>
+
+<script lang="ts">
+  import { Component, Vue, Prop } from 'vue-property-decorator';
+
+  @Component({
+    components: {},
+  })
+  export default class Spinner extends Vue {
+    // props
+    @Prop({ default: 'md' }) size!: string;
+    @Prop({ default: 'primary' }) color!: string;
+    @Prop({ default: '' }) className!: string;
+  }
+</script>
