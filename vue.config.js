f8792d078494d7a371e446aab0a29bbb5042f41b
tempura327 <cks6658324@yahoo.com.tw>
2022-09-29 22:05:43 +0800

    chore

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '/weather/',
+  publicPath: '/weather-forecast/',
 })
